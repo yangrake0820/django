7a251871fb20c1c43b2887135949448526c36e4f
DA9 <yangrake@gmail.com>
2024-04-30 16:17:29 +0900

    가보자고2

---
+++ accounts/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from .models import User
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import redirect
+from .forms import LoginForm
+
+def home(request):
+    user_id = request.session.get('user')
+    if user_id:
+        user = User.objects.get(pk = user_id)
+        return HttpResponse("Hello! %s님" % user)
+    else:
+        return HttpResponse("로그인 해주세요!")
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None) 
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re_password', None)
+        
+        err_data={}
+        if not(username and useremail and password and re_password):
+            err_data['error'] = '모든 값을 입력해주세요.'
+        
+        elif password != re_password:
+            err_data['error'] = '비밀번호가 다릅니다.'
+        
+        else:
+            user = User(
+                username=username,
+                useremail=useremail,
+                password=make_password(password),
+            )
+            user.save()
+
+        return render(request, 'register.html', err_data)
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+    return redirect('/')
---
+++ config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from accounts.views import home
+
+urlpatterns = [
+    path('', home),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+]
